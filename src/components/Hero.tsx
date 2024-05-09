7137161433175aa638b0f884bb4d3b95aa764565
Liezl Mouton <liz.mouton1@gmail.com>
2024-05-09 20:25:11 +0200

    Tagline + Font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,6 +4,7 @@ export default function Hero() {
     return (
         <div className="heroContainer">
             <img className="heroLogo" src="./src/assets/logodark.png" />
+            <h1 className="tagline">Discover, Listen, Connect</h1>
         </div>
     )
 }
