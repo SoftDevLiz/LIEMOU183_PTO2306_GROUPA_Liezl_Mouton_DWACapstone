7ad46cc2054efcaac48f2425eeb96d6035160270
Liezl Mouton <liz.mouton1@gmail.com>
2024-05-09 20:02:18 +0200

    Landing page hero skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import ReactDOM from "react-dom"
+import Hero from "./components/Hero.tsx"
+
 function App() {
-  
+  return (
+    <Hero /> 
+  )
 }
 
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+import "../styles/components.css";
+
+export default function Hero() {
+    return (
+        <div className="heroContainer">
+            <img className="heroLogo" src="./src/assets/logodark.png" />
+        </div>
+    )
+}
