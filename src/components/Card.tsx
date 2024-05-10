d4b521f207ac20dd95ca37398d96dd15e431ed64
Liezl Mouton <liz.mouton1@gmail.com>
2024-05-10 17:12:25 +0200

    Styles card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,15 +4,15 @@ export default function Card() {
     return (
      <footer>
         <div className="card--wrapper">
-        <h1>Explore</h1>
+        <h1 className="footer--heading">Explore</h1>
         <div className="card">
             <img className="card--image" src="./src/assets/podcastExample.png"/>
             <div className="card--info">
-                <span>Ologies by Allie Ward</span>
+                <span className="card--title">Ologies by Allie Ward</span>
                 <span>10 Seasons</span>
                 <span>Scientific</span>
             </div>
-            <p>Ward interviews an expert from a distinct scientific field (somnology, bryology, philematology, etc.).</p>
+            <p className="card--description">Ward interviews an expert from a distinct scientific field (somnology, bryology, philematology, etc.).</p>
         </div>
         </div>
      </footer>
