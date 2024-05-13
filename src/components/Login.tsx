17dc97de9807427aa7907274cfb921c684884b1c
Liezl Mouton <liz.mouton1@gmail.com>
2024-05-13 18:42:38 +0200

    User can login with Supabase

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,6 @@
 import "../styles/components.css";
 import { useState } from "react";
+import supabase from "../utils/supabaseConfig";
 
 const Login: React.FC<{}> = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -8,21 +9,43 @@ const Login: React.FC<{}> = () => {
         setIsOpen(!isOpen);
     }
 
+    const handleLogin = async (email: string) => {
+        try {
+            const response = await supabase.auth.signInWithOtp({
+                email: email
+            });
+
+            if (response.error) {
+                console.error('Error signing in:', response.error.message);
+            } else {
+                console.log('User signed in successfully:', response.data);
+            }
+        } catch (error) {
+            console.error('Error signing in:', error.message);
+        }
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const email = event.currentTarget.email.value;
+        handleLogin(email);
+    }
+
     return (
         <>
-        <button className="hero--button" onClick={toggleModal}>Login</button>
-        {isOpen && (
-        <div className="modal--box">
-            <div className="modal--content">
-                <button className="modal--exit" onClick={toggleModal}></button>
-                <h1 className="modal--header">Welcome back!</h1>
-                <form className="login--form">
-                    <input placeholder="Email"></input>
-                    <input type="submit"></input>
-                </form>
-            </div>
-        </div>
-        )}
+            <button className="hero--button" onClick={toggleModal}>Login</button>
+            {isOpen && (
+                <div className="modal--box">
+                    <div className="modal--content">
+                        <button className="modal--exit" onClick={toggleModal}></button>
+                        <h1 className="modal--header">Welcome back!</h1>
+                        <form className="login--form" onSubmit={handleSubmit}>
+                            <input name="email" placeholder="Email"></input>
+                            <input type="submit"></input>
+                        </form>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
