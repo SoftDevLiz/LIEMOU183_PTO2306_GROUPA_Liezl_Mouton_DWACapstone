833088860bad6add0ca3eecd347c79ffd140fac7
Liezl Mouton <liz.mouton1@gmail.com>
2024-05-15 21:42:18 +0200

    Sets up basic user menu functionality

--- src/components/Home page/Header.tsx
+++ src/components/Home page/Header.tsx
@@ -1,11 +1,25 @@
 import "../../styles/components.css"
+import { useState } from "react";
 
 const Header: React.FC<{}> = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+    const toggleModal = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
+        <>
         <header className="loggedIn--header">
             <img src="./src/assets/logodark.png"  />
-            <button className="user--account"></button>
+            <button className="user--account" onClick={toggleModal}></button>
         </header>
+        {isOpen && (
+            <div className="user--menu">
+            </div>
+        )}
+        </>
+
     )
 }
 
