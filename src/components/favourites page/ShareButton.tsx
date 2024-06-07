598e77a2d68a362850eadf057b7a7ae5e4ac15fe
Liezl Mouton <liz.mouton1@gmail.com>
2024-06-07 10:57:13 +0200

    Create popup for share button

--- src/components/favourites page/ShareButton.tsx
+++ src/components/favourites page/ShareButton.tsx
@@ -1,12 +1,26 @@
-import "../../styles/components.css"
+import React, { useState } from "react";
+import "../../styles/components.css";
 
 const ShareButton: React.FC<{}> = () => {
+    const [showPopup, setShowPopup] = useState(false);
+
+    const copyShareableLink = () => {
+        navigator.clipboard.writeText(window.location.href).then(() => {
+            setShowPopup(true);
+            setTimeout(() => {
+                setShowPopup(false);
+            }, 1000);
+        });
+    };
 
     return (
         <>
-        <button className="share--button"></button>
+            <button className="share--button" onClick={copyShareableLink}></button>
+            {showPopup && (
+                <div className="popup">Link copied</div>
+            )}
         </>
-    )
-}
+    );
+};
 
 export default ShareButton;
