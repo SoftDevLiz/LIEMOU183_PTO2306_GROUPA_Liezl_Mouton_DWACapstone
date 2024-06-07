b391ca494bb675c7549264151693e3fe3b9f1f64
Liezl Mouton <liz.mouton1@gmail.com>
2024-06-07 12:41:30 +0200

    Pull in favourites data and add audio player to shareable page

---
+++ src/components/shareable favourites page/ShareableFaveEpisodeCard.tsx
@@ -0,0 +1,44 @@
+import "../../styles/components.css";
+import { useAudioPlayer } from '../../context/AudioPlayerContext';
+
+interface FavouriteEpisodeProps {
+    key: number,
+    userId: string,
+    show: string,
+    season_id: number;
+    season: string,
+    episodeId: number,
+    title: string,
+    desc: string,
+    audio: string,
+}
+
+const ShareableFaveEpisodeCard: React.FC<FavouriteEpisodeProps> = ({ userId, show, season_id, season, episodeId, title, desc, audio }) => {
+
+    const header = `${show}: ${title}`;
+
+    const { dispatch } = useAudioPlayer();
+
+    const handlePlay = () => {
+      if (userId) {
+        dispatch({ type: 'PLAY', payload: { track: audio, title: header, currentTime: 0 } });
+      } else {
+        console.error('User not logged in');
+      }
+    };
+      
+    return (
+        <div className="fave--episode--wrapper">
+            <h2>{show}</h2>
+            <div className="fave--title--wrapper">
+                <h3 className="card--title">{title}</h3>
+                <button className="play--button" onClick={handlePlay}></button>
+            </div>
+            <h4>Season {season_id}: {season}</h4>
+            <h4>Episode {episodeId}</h4>
+            <p className="card--description">{desc}</p>
+        </div>
+    )
+};
+
+export default ShareableFaveEpisodeCard;
--- src/components/shareable favourites page/ShareableFavePage.tsx
+++ src/components/shareable favourites page/ShareableFavePage.tsx
@@ -1,8 +1,52 @@
 import "../../styles/components.css"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom";
+import supabase from "../../supabaseConfig";
+import ShareableFaveEpisodeCard from "./ShareableFaveEpisodeCard";
 
 const ShareableFavePage: React.FC = () => {
+    const [sharedFavourites, setSharedFavourites] = useState<any[]>([]);
+    const { userId } = useParams<{ userId: string }>();
+
+    useEffect(() => {
+        const fetchFavourites = async () => {
+          if (!userId) return;
+    
+          const { data: fetchedFavourites, error } = await supabase
+            .from('favourites')
+            .select('*')
+            .eq('user_id', userId);
+    
+          if (error) {
+            console.error('Error fetching favourites:', error.message);
+            return;
+          }
+          setSharedFavourites(fetchedFavourites);
+        };
+    
+        if (userId) {
+          fetchFavourites();
+        }
+      }, [userId]);
+
+      console.log(sharedFavourites)
+
     return (
-        <div>hiya</div>
+        <div>
+            <h1>Favourites from your friend!</h1>
+            {sharedFavourites.map((episode, index) => 
+            <ShareableFaveEpisodeCard
+                key={index}
+                userId={userId}
+                show={episode.podcast_title}
+                title={episode.title}
+                season_id={episode.season_id}
+                season={episode.season_title}
+                episodeId={episode.episode_id}
+                desc={episode.description}
+                audio={episode.file}
+            />)}
+        </div>
     )
 }
 
