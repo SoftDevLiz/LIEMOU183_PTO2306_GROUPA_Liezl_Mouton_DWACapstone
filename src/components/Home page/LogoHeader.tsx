b5bfef02bcf6042d1b42d881bf7757ab4d938942
Liezl Mouton <liz.mouton1@gmail.com>
2024-06-07 16:54:35 +0200

    styles home page header

--- src/components/Home page/LogoHeader.tsx
+++ src/components/Home page/LogoHeader.tsx
@@ -1,59 +1,58 @@
-import "../../styles/components.css"
+import "../../styles/components.css";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import supabase from "../../supabaseConfig";
 
 const Header: React.FC<{}> = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [isOpen, setIsOpen] = useState<boolean>(false);
     const [userId, setUserId] = useState<string>("");
 
     const toggleModal = () => {
-        setIsOpen(!isOpen)
-    }
+        setIsOpen(!isOpen);
+    };
 
     useEffect(() => {
         const fetchUser = async () => {
-          const { data: { user } } = await supabase.auth.getUser();
-          if (user) {
-            setUserId(user.id);
-          }
-        }
+            const { data: { user } } = await supabase.auth.getUser();
+            if (user) {
+                setUserId(user.id);
+            }
+        };
 
         fetchUser();
-
     }, []);
 
     const clearListeningHistory = async () => {
         const { data, error } = await supabase
-        .from('watch_history')
-        .delete()
-        .eq('user_id', userId); 
+            .from('watch_history')
+            .delete()
+            .eq('user_id', userId);
 
         if (error) {
             console.error('Error deleting user history:', error);
         } else {
             console.log('User history cleared:', data);
         }
-    }
+    };
 
     return (
         <>
-        <header className="loggedIn--header">
-            <img src="./src/assets/logodark.png"  />
-            <button className="user--account" onClick={toggleModal}></button>
-        </header>
-        {isOpen && (
-            <div className="user--menu">
-                <Link to={'/favourites'}>Favourites</Link>
-                <div className="delete--history--wrapper">
-                    <button onClick={clearListeningHistory} className="delete--history--button">Clear listening history</button>
-                    <img src="/src/assets/trash.png" />
+            <header className="loggedIn--header">
+                <img src="./src/assets/logodark.png" />
+                <button className="user--account" onClick={toggleModal}></button>
+            </header>
+            {isOpen && (
+                <div className="user--menu">
+                    <Link to={'/favourites'}>Favourites</Link>
+                    <div className="delete--history--wrapper">
+                        <button onClick={clearListeningHistory} className="delete--history--button">
+                            Clear listening history
+                        </button>
+                    </div>
                 </div>
-            </div>
-        )}
+            )}
         </>
-
-    )
-}
+    );
+};
 
 export default Header;
