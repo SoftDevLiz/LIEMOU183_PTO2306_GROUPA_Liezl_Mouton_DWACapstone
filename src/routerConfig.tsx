a7a7082498f848bc0d10e2c521ec85606a4d71dd
Liezl Mouton <liz.mouton1@gmail.com>
2024-06-07 18:43:04 +0200

    fix pathing

--- src/routerConfig.tsx
+++ src/routerConfig.tsx
@@ -1,10 +1,10 @@
-import LandingPage from '../src/components/landing page/LandingPage';
-import HomePage from '../src/components/home page/HomePage';
-import Err404Page from '../src/components/error page/Err404Page';
-import ShowDetailsPage from '../src/components/show details page/ShowDetailsPage';
-import ShowSeasonsPage from '../src/components/show seasons page/ShowSeasonsPage';
-import FavouritesPage from '../src/components/favourites page/FavouritesPage';
-import ShareableFavePage from '../src/components/shareable favourites page/ShareableFavePage';
+import LandingPage from '../src/components/landing page/LandingPage.tsx'
+import HomePage from '../src/components/home page/HomePage.tsx';
+import Err404Page from '../src/components/error page/Err404Page.tsx';
+import ShowDetailsPage from '../src/components/show details page/ShowDetailsPage.tsx';
+import ShowSeasonsPage from '../src/components/show seasons page/ShowSeasonsPage.tsx';
+import FavouritesPage from '../src/components/favourites page/FavouritesPage.tsx';
+import ShareableFavePage from '../src/components/shareable favourites page/ShareableFavePage.tsx';
 
 
 export const routes = [
