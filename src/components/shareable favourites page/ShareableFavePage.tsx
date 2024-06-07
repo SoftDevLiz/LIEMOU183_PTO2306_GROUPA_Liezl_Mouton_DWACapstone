fb3023c04ec82e91032f7401b25af1351da3e613
Liezl Mouton <liz.mouton1@gmail.com>
2024-06-07 17:49:22 +0200

    Styles shareable fave page

--- src/components/shareable favourites page/ShareableFavePage.tsx
+++ src/components/shareable favourites page/ShareableFavePage.tsx
@@ -37,10 +37,10 @@ const ShareableFavePage: React.FC = () => {
       }
 
     return (
-        <div>
+        <div className="shareable--fave--page">
             <div className="shareable--fave--header">
             <img src="/src/assets/logodark.png" onClick={navigateToLanding} />
-            <h1 className="hero--tagline">Discover, Listen, Connect</h1>
+            <h1>Discover, Listen, Connect</h1>
             <h1>Favourites from your friend!</h1>
             </div>
             {sharedFavourites.map((episode, index) => 
